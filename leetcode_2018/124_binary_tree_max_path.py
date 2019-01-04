d6c88e8fe3474dd499632d932ee6613f60e9d714
Sunny Changediya <schanged@amazon.com>
2019-01-03 17:56:57 -0800

    added iterative method: stack + postorder

--- leetcode_2018/124_binary_tree_max_path.py
+++ leetcode_2018/124_binary_tree_max_path.py
@@ -15,7 +15,7 @@ Input: [1,2,3]
       / \
      2   3
 
-Output: 6
+Output: 6 [2, 3, 1]
 
 Example 2:
 
@@ -27,7 +27,7 @@ Input: [-10,9,20,null,null,15,7]
     /  \
    15   7
 
-Output: 42
+Output: 42 [15, 20, 7]
 
 
 """
@@ -39,29 +39,61 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-class Solution:
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        self.result_val = -99999999
-        def helper(node):
-            if not node:
-                return 0
-            left = helper(node.left)
-            print("left = %s | node = %s | " %(left, node.val))
-            right = helper(node.right)
-            print("right = %s | node = %s | " %(right, node.val))
-            self.result_val = max(self.result_val, left + node.val + right)
-            print("returning something: %s" %(max(node.val + max(left, right), 0)))
-            print("=============================")
-            return max(node.val + max(left, right), 0)
-        print("Start.....")
-        helper(root)
-        return self.result_val
 
-root = TreeNode(-10)
+# Recursive
+def maxPathSum(self, root):
+    """
+    :type root: TreeNode
+    :rtype: int
+    """
+    self.result_val = -99999999
+    def helper(node):
+        if not node:
+            return 0
+        left = helper(node.left)
+        # print("left = %s | node = %s | " %(left, node.val))
+        right = helper(node.right)
+        # print("right = %s | node = %s | " %(right, node.val))
+        self.result_val = max(self.result_val, left + node.val + right)
+        # print("returning something: %s" %(max(node.val + max(left, right), 0)))
+        # print("=============================")
+        return max(node.val + max(left, right), 0)
+    # print("Start.....")
+    helper(root)
+    return self.result_val
+
+# Iterative stack + postorder
+def maxPathSum(self, root):
+    """
+    :type root: TreeNode
+    :rtype: int
+    """
+    if not root:
+        return
+
+    def helper(node):
+        stack = [node]
+        result = []
+        while stack:
+            curr = stack.pop()
+            result.append(curr)
+            if curr.left:
+                stack.append(curr.left)
+            if curr.right:
+                stack.append(curr.right)
+        return result
+    # postorder traversal using two stacks
+    result = helper(root)
+    final = -999999
+    hashmap = {None: 0}
+    for node in result[::-1]:
+        left = max(hashmap.get(node.left), 0)
+        right = max(hashmap.get(node.right), 0)
+        hashmap[node] = max(left, right) + node.val
+        final = max(left+right+node.val, final)
+    return final
+
+root = TreeNode(10)
 root.left = TreeNode(9)
 root.right = TreeNode(20)
 root.right.right = TreeNode(7)
