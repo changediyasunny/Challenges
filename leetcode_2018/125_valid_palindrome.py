f61ad9242f492cbe4fad0e674989104dfe41201d
Sunny Changediya <schanged@amazon.com>
2019-01-03 17:57:28 -0800

    indentation fix

--- leetcode_2018/125_valid_palindrome.py
+++ leetcode_2018/125_valid_palindrome.py
@@ -1,20 +1,36 @@
-class Solution:
-        def isPalindrome(self, s):
-                    """
-                            :type s: str
-                                    :rtype: bool
-                                            """
-                                                    left = 0
-                                                            right = len(s) - 1
-                                                                    while left < right:
-                                                                                    
-                                                                                    while left < right and not s[left].isalnum():
-                                                                                                        left += 1
-                                                                                                                    while left < right and not s[right].isalnum():
-                                                                                                                                        right -= 1
-                                                                                                                                                    
-                                                                                                                                                                if s[left].lower() != s[right].lower():
-                                                                                                                                                                                    return False
-                                                                                                                                                                                            left += 1
-                                                                                                                                                                                                        right -= 1
-                                                                                                                                                                                                                return True
+"""
+125. Valid Palindrome
+
+Given a string, determine if it is a palindrome, considering only alphanumeric
+characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+
+"""
+
+def isPalindrome(self, s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+    return True
