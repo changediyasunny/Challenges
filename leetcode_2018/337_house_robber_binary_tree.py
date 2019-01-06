bf6739889b45c5634ca2b7abf31a4fd91e139e1b
Sunny Changediya <schanged@amazon.com>
2019-01-05 16:47:39 -0800

    added iterative solution

--- leetcode_2018/337_house_robber_binary_tree.py
+++ leetcode_2018/337_house_robber_binary_tree.py
@@ -1,20 +1,21 @@
 """
 337. House Robber III
 
-The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root."
-Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this
-place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+The thief has found himself a new place for his thievery again. There is only one entrance to this area,
+called the "root." Besides the root, each house has one and only one parent house. After a tour,
+the smart thief realized that "all houses in this place forms a binary tree". It will automatically
+contact the police if two directly-linked houses were broken into on the same night.
 
 Determine the maximum amount of money the thief can rob tonight without alerting the police.
 
 Example 1:
 Input: [3,2,3,null,3,null,1]
 
-     3
+     3*
     / \
    2   3
     \   \
-     3   1
+     3*   1*
 
 Output: 7
 Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
@@ -24,13 +25,15 @@ Input: [3,4,5,1,3,null,1]
 
      3
     / \
-   4   5
+   4*   5*
   / \   \
  1   3   1
 
 Output: 9
 Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
 
+Space: O(N)
+running time: O(N)
 """
 
 
@@ -41,6 +44,24 @@ class TreeNode:
         self.left = None
         self.right = None
 
+# Iterative
+def rob(self, root):
+    stack = [(0, root)]
+    # {Node: (rob, don't rob)}
+    data = {None: (0, 0)}
+    while stack:
+        seen, node = stack.pop()
+        if node is None:
+            continue
+        if not seen:
+            stack.extend([(1, node), (0, node.left), (0, node.right)])
+        else:
+            rob_root = node.val + data[node.left][1] + data[node.right][1]
+            dont_rob_root = max(data[node.left]) + max(data[node.right])
+            data[node] = max(rob_root, dont_rob_root)
+    return max(data[root])
+
+# Recursive
 class Solution:
     def rob(self, root):
         """
@@ -57,7 +78,7 @@ class Solution:
             left = helper(node.left)
             right = helper(node.right)
             result = []
-            result.insert(0, max(left[0], left[1]) + max(right[0], right[1]) )
+            result.insert(0, max(left) + max(right) )
             result.insert(1, node.val + left[0] + right[0])
             return result
 
