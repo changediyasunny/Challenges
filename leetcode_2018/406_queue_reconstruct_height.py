1323ac48bace45d1e1fb13eb198a2027f2fcf614
Sunny Changediya <schanged@amazon.com>
2019-01-09 16:47:13 -0800

    new solution

--- leetcode_2018/406_queue_reconstruct_height.py
+++ leetcode_2018/406_queue_reconstruct_height.py
@@ -1,52 +1,49 @@
 """
 406. Queue Reconstruction by Height
 
-Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), 
-where h is the height of the person and k is the number of people in front of this person who have a height greater 
-than or equal to h. Write an algorithm to reconstruct the queue.
-
+Suppose you have a random list of people standing in a queue. Each person is described by a
+pair of integers (h, k), where h is the height of the person and k is the number of people
+in front of this person who have a height greater than or equal to h. Write an algorithm to
+reconstruct the queue.
 Note:
 The number of people is less than 1,100.
 
 Example
-
 Input:
 [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
 
 Output:
 [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
 
+Pick out tallest group of people and sort them in a subarray (S). Since there's no other
+groups of people taller than them, therefore each guy's index will be just as same as his k value.
+For 2nd tallest group (and the rest), insert each one of them into (S) by k value. So on and so forth.
+
+E.g.
+input: [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+subarray after step 1: [[7,0], [7,1]]
+subarray after step 2: [[7,0], [6,1], [7,1]]
 """
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 ### Solution: 1
 def reconstructQueue(people):
-    if not people: return []
-
-    # obtain everyone's info
-    # key=height, value=k-value, index in original array
+    if not people:
+        return []
     peopledct, height, res = {}, [], []
-    
-    for i in xrange(len(people)):
-        p = people[i]
+    for ind, p in enumerate(people):
+        h, k = p[0], p[1]
         if p[0] in peopledct:
-            peopledct[p[0]] += (p[1], i),
+            peopledct[h] += (k, ind),
         else:
-            peopledct[p[0]] = [(p[1], i)]
-            height += p[0],
-
-    pp.pprint(peopledct)
-    height.sort()      # here are different heights we have
-    pp.pprint(height)
+            peopledct[h] = [(k, ind)]
+            height.append(h)
+    height.sort()
     # sort from the tallest group
     for h in height[::-1]:
-        peopledct[h].sort()
-        print("....................")
-        print("%s" %peopledct[h])
-        print("....................")
-        for p in peopledct[h]:
-            res.insert(p[0], people[p[1]])
+        for k, ind in sorted(peopledct[h]):
+            res.insert(k, people[ind])
     return res
 
 ### Solution: 2
