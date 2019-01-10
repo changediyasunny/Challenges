52aef5f3c2223f839c505dfde6fa8212748e648e
Sunny Changediya <schanged@amazon.com>
2019-01-09 16:49:57 -0800

    Indentation fix

--- leetcode_2018/692_top_k_frequent_words.py
+++ leetcode_2018/692_top_k_frequent_words.py
@@ -1,9 +1,7 @@
 """
-
 692. Top K Frequent Words
 
 Given a non-empty list of words, return the k most frequent elements.
-
 Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, then the word with the lower alphabetical order comes first.
 
 Example 1:
@@ -18,19 +16,15 @@ Output: ["the", "is", "sunny", "day"]
 Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
     with the number of occurrence being 4, 3, 2 and 1 respectively.
 
-
 Note:
 You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
 Input words contain only lowercase letters.
 Follow up:
 Try to solve it in O(n log k) time and O(n) extra space.
 
-
-Time Complexity: O(Nlogk), where N is the length of words. We count the frequency of each word in O(N) time, then we add N words
-to the heap, each in O(logk) time. Finally, we pop from the heap up to k times. As k≤N, this is O(Nlogk) in total.
+Time Complexity: O(Nlogk), where N is the length of words. We count the frequency of each word in O(N) time, then we add N words to the heap, each in O(logk) time. Finally, we pop from the heap up to k times. As k≤N, this is O(Nlogk) in total.
 
 Space Complexity: O(N), the space used to store our count dict.
-
 """
 
 def topKFrequent(words, k):
