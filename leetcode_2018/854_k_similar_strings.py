4e06fc1658a7a762e561b71146cfb7ed030f0ef7
Sunny Changediya <schanged@amazon.com>
2019-05-09 22:59:33 -0700

    combined 2 problems. min k and smaller k

--- leetcode_2018/854_k_similar_strings.py
+++ leetcode_2018/854_k_similar_strings.py
@@ -51,7 +51,7 @@ The length of string s and t are guaranteed to be the same.
 time: 2^(N + W) where W is length of alphabet
 space: O(N * t) where N is length of string and t is time complexity given above
 """
-def kSimilarity(strs1, strs2, k):
+def kSimilarity(strs1, strs2, k, flag=1):
     def neighbors(strs):
         for i, c in enumerate(strs):
             if c != strs2[i]:
@@ -65,17 +65,24 @@ def kSimilarity(strs1, strs2, k):
                 temp_strs[j], temp_strs[i] = temp_strs[i], temp_strs[j]
         return data
 
-    queue = deque([strs1])
+    queue = [strs1]
     seen = {strs1: 0}
     while queue:
-        strs = queue.popleft()
+        strs = queue.pop(0)
         if strs == strs2:
-            if seen[strs] > k:
-                return False
-            return True
+            if flag == 1:
+                # to return smallest k-similar strings
+                return seen[strs]
+            else:
+                # to return if they are k-similar
+                if seen[strs] > k:
+                    return False
+                return True
 
         for tmp_strs in neighbors(strs):
             print(tmp_strs)
             if tmp_strs not in seen:
                 seen[tmp_strs] = seen[strs] + 1
                 queue.append(tmp_strs)
+
+print(kSimilarity(strs1='ab', strs2='ba', k=5, flag=1))
