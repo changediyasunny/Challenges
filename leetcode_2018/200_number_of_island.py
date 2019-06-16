a8be466401b424bef96d66d49646ee166a74e37b
Sunny Changediya <schanged@amazon.com>
2019-06-16 07:30:55 -0700

    adding new maps method

--- leetcode_2018/200_number_of_island.py
+++ leetcode_2018/200_number_of_island.py
@@ -1,27 +1,24 @@
 """
 200. Number of Islands
 
-Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting
-adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally
+or vertically. You may assume all four edges of the grid are all surrounded by water.
 
 Example 1:
-
 Input:
 11110
 11010
 11000
 00000
-
 Output: 1
 
 Example 2:
-
 Input:
 11000
 11000
 00100
 00011
-
 Output: 3
 
 DFS & BFS: Find strongly connected components of graph
@@ -61,3 +58,18 @@ def numIslands(self, grid):
                 count += 1
                 changeLandWater(grid, i, j)
     return count
+
+# Using map function.
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        def sink(i, j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
+                grid[i][j] = '0'
+                map(sink, (i+1, i-1, i, i), (j, j, j+1, j-1))
+                return 1
+            return 0
+        return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
