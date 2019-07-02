f08129cd366b7d1a924934a44a27c1ec91251742
Sunny Changediya <schanged@amazon.com>
2019-07-01 19:00:30 -0700

    new code

--- leetcode_2018/685_redundant_Connections_2.py
+++ leetcode_2018/685_redundant_Connections_2.py
@@ -32,7 +32,66 @@ Explanation: The given directed graph will be like this:
      ^    |
      |    v
      4 <- 3
+
+Case-1:
+There is a loop in the graph, and no vertex has more than 1 parent.
+Example:
+Input: [[1,2], [2,3], [3,4], [4,1], [1,5]]
+Output: [4,1]
+
+Case-2:
+A vertex has more than 1 parent, but there isn't a loop in the graph.
+Example:
+Input: [[1,2], [1,3], [2,3]]
+Output: [2,3]
+
+Case-3:
+A vertex has more than 1 parent, and is part of a loop.
+Example:
+Input: [[2,1], [3,1], [4,2], [1,4]]
+Output: [2,1]
+
 """
+class Solution(object):
+    def findRedundantDirectedConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        parent = {}
+        candidate = []
+
+        k = list(range(len(edges)+1))
+        def find(x):
+            while k[x] != x:
+                x = k[x]
+            return x
+
+        def detect_cycle(edge):
+            u, v = edge
+            while u != v and u in parent:
+                u = parent[u]
+            return u == v
+
+        for u, v in edges:
+            if v in parent:
+                # these are culprits and assume to have multiple parents
+                candidate.append((parent[v], v))
+                candidate.append((u, v))
+            else: parent[v] = u
+
+        if candidate:
+            # some node has multiple parents or cycle exists
+            return candidate[0] if detect_cycle(candidate[0]) else candidate[1]
+        else:
+            # ususal redundant connections
+            for edge in edges:
+                u, v = edge
+                u = find(u)
+                v = find(v)
+                if u == v:
+                    return edge
+                k[u] = k[v]
 
 
 class Solution:
