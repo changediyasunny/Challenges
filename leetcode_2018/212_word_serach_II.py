dd215dfd67f2ad43fbad49fbdfd537c2c205c2ee
Sunny Changediya <schanged@amazon.com>
2019-07-01 18:59:17 -0700

    imporved module

--- leetcode_2018/212_word_serach_II.py
+++ leetcode_2018/212_word_serach_II.py
@@ -25,68 +25,57 @@ O(len(words) * avg-len-words): build a TRIE
 space: O(len(words) * avg-len-words)
 """
 
-class TrieNode():
+class Trie(object):
     def __init__(self):
         self.data = {}
-        self.isEnd = False
+        self.end = False
 
-def insert(root, word):
-        """
-        Inserts a word into the trie.
-        :type word: str
-        :rtype: void
-        """
-        current = root
+    def insert(self, word):
+        current = self
         for w in word:
-            temp_node = current.data.get(w, None)
-            if temp_node is None:
-                temp_node = TrieNode()
-                current.data[w] = temp_node
-            current = temp_node
-        current.isEnd = True
+            if w not in current.data:
+                current.data[w] = Trie()
+            current = current.data[w]
+        current.end = True
 
-def findWords(board, words):
-    """
-    :type board: List[List[str]]
-    :type words: List[str]
-    :rtype: List[str]
-    """
-    # create TRIE
-    root = TrieNode()
-    for word in words:
-        insert(root, word)
+class Solution(object):
+    def findWords(self, board, words):
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        def dfs(board, node, i, j, path, output):
 
-    # search Trie & DFS
-    rows = len(board)
-    cols = len(board[0])
-    result = []
-    for i in range(rows):
-        for j in range(cols):
-            dfs(board, root, i, j, "", result)
-    return result
+            if node.end:
+                output.append(path)
+                node.end = False
+            if (i<0 or i >=len(board) or j<0 or j>= len(board[0])):
+                return
+            w = board[i][j]
+            node = node.data.get(w, None)
+            if node is None:
+                return
 
-def dfs(board, root, i, j, strs, result):
-    # print(board)
-    # print(i, j, strs, result)
-    # print("=======================")
-    node = root
-    # end of word reached & string found
-    if node.isEnd:
-        result.append(strs)
-        node.isEnd = False
-    if (i < 0 or i >= len(board) or j <0 or j >= len(board[0])):
-        return
-    w = board[i][j]    # valid cahracter
-    node = node.data.get(w, None)
-    if node is None:
-        return
-    board[i][j] = '#'
-    dfs(board, node, i+1, j, strs+w, result)
-    dfs(board, node, i-1, j, strs+w, result)
-    dfs(board, node, i, j+1, strs+w, result)
-    dfs(board, node, i, j-1, strs+w, result)
-    board[i][j] = w
+            board[i][j] = '#'
+            dfs(board, node, i+1, j, path+w, output)
+            dfs(board, node, i-1, j, path+w, output)
+            dfs(board, node, i, j-1, path+w, output)
+            dfs(board, node, i, j+1, path+w, output)
+            board[i][j] = w
 
+        # create Trie
+        root = Trie()
+        for word in words:
+            root.insert(word)
+
+        rows = len(board)
+        cols = len(board[0])
+        result = []
+        for i in range(rows):
+            for j in range(cols):
+                dfs(board, root, i, j, "", result)
+        return result
 
 words = ["oath","pea","eat","rain"]
 board = [
