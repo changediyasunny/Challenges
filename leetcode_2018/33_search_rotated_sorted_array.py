108df98f6cdeefcce39893ca434bd7311dceb206
schanged <schanged@hawk.iit.edu>
2019-10-05 15:41:27 -0700

    new improved binary search algorithm

--- leetcode_2018/33_search_rotated_sorted_array.py
+++ leetcode_2018/33_search_rotated_sorted_array.py
@@ -50,4 +50,30 @@ def search(nums, target):
             high = mid - 1
     return -1
 
-print(search([4,5,6,7,0,1,2], 0))
+# Single pass solution
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low, high = 0, len(nums)-1
+        while low <= high:
+            mid = (low + high)//2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                # <= is necessary because [3,1], low & mid falls on same index
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
