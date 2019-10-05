28e9bcd9e4a29a02787eef04dbacfaf73f8eed4a
schanged <schanged@hawk.iit.edu>
2019-10-05 15:40:07 -0700

    removed main() method

--- leetcode_2018/1_two_sums.py
+++ leetcode_2018/1_two_sums.py
@@ -1,12 +1,11 @@
 """
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+1. Two sums
 
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 Example:
-
 Given nums = [2, 7, 11, 15], target = 9,
-
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 
@@ -24,11 +23,3 @@ def two_sums(nums, target):
             return [ hash_map[nums[i]], i ]
         else:
             hash_map[t] = i
-        print(hash_map)
-
-def main():
-    result = two_sums([2,7,9,11], 9)
-    print(result)
-
-if __name__ == '__main__':
-    main()
